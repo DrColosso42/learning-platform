ead22d3a8c3b21d38391b71d298d4be808ee18f9
daispod <nenad@dev.com>
2025-10-01 10:17:04 +0200

    Add @Builder annotation to CreateStudySessionRequest

--- backend-springboot/src/main/java/com/learningadvisor/backend/dto/studysession/CreateStudySessionRequest.java
+++ backend-springboot/src/main/java/com/learningadvisor/backend/dto/studysession/CreateStudySessionRequest.java
@@ -2,6 +2,7 @@ package com.learningadvisor.backend.dto.studysession;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CreateStudySessionRequest {
 
     @NotNull(message = "Question set ID is required")
